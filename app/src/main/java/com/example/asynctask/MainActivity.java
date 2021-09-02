15a950b046c228f09b30e443925baa16b645dc6a
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-09-02 23:37:12 +0300

    Updated-1.

---
+++ app/src/main/java/com/example/asynctask/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.asynctask;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity
+{
+    TextView myView;
+    MyTask mt;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myView = (TextView)findViewById(R.id.textView2);
+    }
+
+    public  void onClick(View v)
+    {
+        mt = new MyTask();
+        mt.execute();
+    }
+
+    class MyTask extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+            myView.setText("Start");
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids)
+        {
+            try
+            {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid)
+        {
+            super.onPostExecute(aVoid);
+            myView.setText("End");
+        }
+    }
+}
